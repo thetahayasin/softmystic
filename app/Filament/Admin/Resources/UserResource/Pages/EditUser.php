c485d16d6ff44b94738a3d945937aec4e5ebc716
Taha Yasin <causebroz7@gmail.com>
2025-06-28 20:32:57 +0500

    user resource fixed

--- app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,8 +16,22 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            //Actions\DeleteAction::make(),
-        ];
+            Actions\DeleteAction::make()
+            ->visible(fn ($record) => $record->id !== 1)
+            ->before(function (Actions\DeleteAction $action, User $record) {
+                if ($record->softwares()->exists()) {
+                    Notification::make()
+                        ->danger()
+                        ->title('Failed to delete!')
+                        ->body('This user has softwares related to it first remove them or deactivate the user')
+                        ->persistent()
+                        ->send();
+         
+                        // This will halt and cancel the delete action modal.
+                        $action->cancel();
+                }
+            })      
+          ];
     }
 
     protected function mutateFormDataBeforeSave(array $data): array
