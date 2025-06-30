49679acd8f00fda091df26a24107cfc9af625517
Taha Yasin <causebroz7@gmail.com>
2025-06-30 21:49:05 +0500

    bug fixes

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -148,10 +148,7 @@
 
                         <!-- X (Twitter) -->
                         <a href="https://twitter.com/intent/tweet?url={{ urlencode($software->url) }}&text={{ $meta_title ?? '' }} - {{ $ads->site_name }}" target="_blank" rel="noopener noreferrer" class="btn btn-circle bg-base-300 hover:bg-base-300 text-white">
-                            <svg fill="#000000" class="w-6 h-6 fill-current text-base-content" viewBox="0 0 24 24" id="twitter" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-                                <path id="primary" d="M21.63,6.92a1,1,0,0,0-.83-.2,8.31,8.31,0,0,1-1.1.17,4.49,4.49,0,0,0-8.55.52c-2.73.67-5.4-2.07-5.43-2.1a1,1,0,0,0-1-.27,1,1,0,0,0-.7.8,11.69,11.69,0,0,0,1.51,8.67A7.25,7.25,0,0,1,3,15a1,1,0,0,0-.89,1.46C2.3,16.83,4.18,20,11.58,20c5.84,0,8-5.2,8.36-8.38A4,4,0,0,0,22,7.7,1,1,0,0,0,21.63,6.92Z" style="fill: rgb(0, 0, 0);">
-                                </path>
-                            </svg>
+                            <svg fill="#000000" class="w-6 h-6 fill-current text-base-content" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="M22,5.8a8.49,8.49,0,0,1-2.36.64,4.13,4.13,0,0,0,1.81-2.27,8.21,8.21,0,0,1-2.61,1,4.1,4.1,0,0,0-7,3.74A11.64,11.64,0,0,1,3.39,4.62a4.16,4.16,0,0,0-.55,2.07A4.09,4.09,0,0,0,4.66,10.1,4.05,4.05,0,0,1,2.8,9.59v.05a4.1,4.1,0,0,0,3.3,4A3.93,3.93,0,0,1,5,13.81a4.9,4.9,0,0,1-.77-.07,4.11,4.11,0,0,0,3.83,2.84A8.22,8.22,0,0,1,3,18.34a7.93,7.93,0,0,1-1-.06,11.57,11.57,0,0,0,6.29,1.85A11.59,11.59,0,0,0,20,8.45c0-.17,0-.35,0-.53A8.43,8.43,0,0,0,22,5.8Z"/></svg>
                         </a>
 
                         <!-- LinkedIn -->
