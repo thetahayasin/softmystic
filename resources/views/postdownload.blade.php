1e340c172bf8c0ce5b945e4371c3c6e049bab889
Taha Yasin <59453558+thetahayasin@users.noreply.github.com>
2025-07-04 17:20:00 +0500

    Update postdownload.blade.php

--- resources/views/postdownload.blade.php
+++ resources/views/postdownload.blade.php
@@ -21,7 +21,7 @@
 
     <!-- ad 1 -->
     @if($ads['download_page_ad'] != null)
-        <div class="card bg-base-200 hover:bg-base-300 transition duration-300 ease-in-out rounded-2xl mb-10 mt-10 max-h-[90px]">
+        <div class="card bg-base-200 transition duration-300 ease-in-out rounded-2xl mb-10 mt-10 max-h-[90px]">
             {!! $ads['download_page_ad'] !!}
         </div>
     @endif
@@ -66,7 +66,7 @@
 
     <!-- ad 2 -->
     @if($ads['download_page_ad_2'] != null)
-        <div class="card bg-base-200 hover:bg-base-300 transition duration-300 ease-in-out rounded-2xl mt-10 max-h-[90px]">
+        <div class="card bg-base-200 transition duration-300 ease-in-out rounded-2xl mt-10 max-h-[90px]">
             {!! $ads['download_page_ad_2'] !!}
         </div>
     @endif
