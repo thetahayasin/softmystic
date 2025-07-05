5745e4fa25226184dd741a79a38ac47f0f345bb9
Taha Yasin <causebroz7@gmail.com>
2025-07-05 22:15:42 +0500

    bug fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,9 @@
     <title>@yield('meta_title', 'Your Site Name - Best Apps & Downloads')</title>
     <meta name="description" content="@yield('meta_description', 'Discover the best apps, featured downloads, latest updates, and new releases on Your Site Name.')" />
     <link rel="icon" href="{{ asset('storage/site_images/site_favicon.ico') }}" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.24/dist/full.min.css" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" />
+    <link href="{{ asset('css/daisyui/full.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('js/tailwind/full.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/fontawesome/all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/app/app.css') }}" />
     @yield('styles')
     @livewireStyles
@@ -29,7 +29,7 @@
 
 </main>
 <script src="{{ asset('js/app/app.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="{{ asset('js/fontawesome/all.min.js') }}"></script>
 @yield('scripts')
 @livewireScripts
 {!! $settings->footer_code !!}
