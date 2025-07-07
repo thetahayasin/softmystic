daf9e6fd14d298e43ba330b75a0eb878952bad29
Taha Yasin <59453558+thetahayasin@users.noreply.github.com>
2025-07-07 17:35:23 +0500

    Update single.blade.php

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -70,14 +70,17 @@
         "@type": "Offer",
         "price": 0.00,
         "priceCurrency": "USD"
-    },
-    "aggregateRating": {
+    }
+      @if($software->total_ratings > 0)
+      ,
+      "aggregateRating": {
         "@type": "AggregateRating",
         "ratingValue": "{{ number_format($software->average_rating, 1) }}",
         "ratingCount": "{{ $software->total_ratings }}",
         "bestRating": "10",
         "worstRating": "2"
-    }
+      }
+      @endif
     
     }
     </script>
@@ -116,7 +119,7 @@
             <li class="text-base-content/70">{{ $software->name }} {{ $software->version }}</li>
         </ul>
         <div class="p-0">
-                <button class="" onclick="share_modal.showModal()" aria-label="Share Button">
+                <button onclick="share_modal.showModal()" aria-label="Share Button">
                     <svg class="w-6 h-6 fill-current text-base-content" viewBox="-5 -5 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-share"><path d='M8 3.414v5.642a1 1 0 1 1-2 0V3.414L4.879 4.536A1 1 0 0 1 3.464 3.12L6.293.293a.997.997 0 0 1 1.414 0l2.829 2.828A1 1 0 1 1 9.12 4.536L8 3.414zM3 6a1 1 0 1 1 0 2H2v4h10V8h-1a1 1 0 0 1 0-2h1a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h1z' /></svg>
                 </button>
 
